4c0de0024446fd81531a8623c3c3e68ca1e23d23
Yog-Sothub <62727181+Yog-Sothub@users.noreply.github.com>
2021-08-20 17:12:25 +0200

    Add files via upload

---
+++ base-install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+ln -sf /usr/share/zoneinfo/Europe/Stockholm /etc/localtime
+hwclock --systohc
+sed -i '444s/.//' /etc/locale.gen
+locale-gen
+echo "LANG=sv_SE.UTF-8" >> /etc/locale.conf
+echo "KEYMAP=sv-latin1" >> /etc/vconsole.conf
+echo "pomelo" >> /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1       localhost" >> /etc/hosts
+echo "127.0.1.1 pomelo.localdomain pomelo" >> /etc/hosts
+echo root:password:password | chpasswd
+
+# paket att installera
+pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-lts-headers avahi xdg-user-dirs xdg-utils gvfs nfs-utils inetutils dnsutils alsa-utils pipewire pipewire-pulse pipewire-jack bash-completion rsync acpi acpid acpi-call tlp ufw flatpak terminus-font pamixer xf86-video-intel mesa libva-intel-driver
+
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+
+systemctl enable NetworkManager
+systemctl enable tlp
+systemctl enable reflector.timer
+systemctl enable fstrim.timer
+systemctl enable ufw
+systemctl enable acpid
+
+useradd -m andreo
+echo andreo:password | chpasswd
+usermod -mG wheel andreo
+
+echo "andreo ALL=(ALL) ALL" >> /etc/sudoers.d/andreo
+
+
---
+++ dwm-install.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+sudo timedatectl set-ntp true
+sudo hwclock --systohc
+
+sudo reflector -c Sweden -a 6 --protocol https --protocol http --sort rate --save /etc/pacman.d/mirrorlist
+
+sudo pacman -S xorg thunar firefox redshift newsboat picom mpc mpv mpd ncmpcpp ffmpeg youtube-dl bat dunst arc-solid-gtk-theme papirus-icon-theme fish starship keepassxc htop neofetch ttf-dejavu gnu-free-fonts ttf-liberation noto-fonts ttf-hack ttf-fira-code adobe-source-code-pro-fonts noto-fonts-cjk noto-fonts-emoji ttf-fira-mono
+
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si 
+cd
+
+# DWM
+git clone https://github.com/Yog-Sothub/dwm.git
+cd dwm
+make && sudo make clean install
+cd
+
+# ST
+git clone https://github.com/Yog-Sothub/st.git
+cd st
+make && sudo make clean install
+cd
+
+#DMENU
+git clone https://github.com/Yog-Sothub/dmenu.git
+cd dmenu
+make && sudo make clean install
+cd
+
+# Dwmblocks
+git clone https://github.com/Yog-Sothub/dwmblocks.git
+cd dwmblocks
+make && sudo make clean install
+cd
+
+# Void configs
+git clone https://github.com/Yog-Sothub/void.git
+
+cp ~/void/xinitrc .xinitrc
+cp ~/void/Xdefaults .Xdefaults
+cp ~/void/bash_profile .bash_profile
+mkdir ~/.local/bin/ 
+sudo mv ~/void/local/bin/statusbar ~/.local/bin/
+
+mv ~/void/config/dunst ~/.config
+mv ~/void/config/mpd ~/.config
+mv ~/void/config/mpv ~/.config
+mv ~/void/config/ncmpcpp ~/.config
+mv ~/void/config/mpc ~/.config
+mv ~/void/config/newsboat ~/.config
+mv ~/void/config/picom ~/.config
+mv ~/void/config/redshift ~/.config
+mv ~/void/config/x11 ~/.config
+mv ~/void/config/nvim ~/.config
+
+
+
+
+
