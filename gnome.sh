4e2719fd7a09a23906153c704b0d8a413ba70e46
Yog-Sothub <62727181+Yog-Sothub@users.noreply.github.com>
2021-09-25 15:40:11 +0200

    Add files via upload

---
+++ gnome.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo pacman -S xorg gdm gnome gnome-extra firefox mpv ffmpeg youtube-dl bat fish starship keepassxc ttf-dejavu gnu-free-fonts ttf-liberation ttf-hack ttf-fira-code adobe-source-code-pro-fonts noto-fonts-cjk noto-fonts-emoji ttf-fira-mono
